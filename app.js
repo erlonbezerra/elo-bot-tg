3736f277d3fec6ec8d4497c9e322a57075781c6d
Erlon Bezerra - F2976049 <erlon.bezerra@bb.com.br>
2018-10-18 13:56:12 -0300

    ajustes de nome

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var tbot = require('node-telegram-bot-api');
+var TelegramBot = require('node-telegram-bot-api');
 
 const TG_TOKEN = process.env.TG_TOKEN;
 
@@ -8,14 +8,14 @@ var app = express();
 var port = process.env.PORT || 8080;
 app.set('port', port);
 
-var telegramBot = new tbot(TG_TOKEN, { polling: true });
+var bot = new TelegramBot(TG_TOKEN, { polling: true });
 
-telegramBot.on('message', function (msg) {
+bot.on('message', function (msg) {
     var chatId = msg.chat.id;
-    telegramBot.sendMessage(chatId, 'Received your message');
+    bot.sendMessage(chatId, 'Received your message');
 });
 
-telegramBot.onText(/\/echo (.+)/, (msg, match) => {
+bot.onText(/\/echo (.+)/, (msg, match) => {
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
     // of the message
@@ -24,7 +24,7 @@ telegramBot.onText(/\/echo (.+)/, (msg, match) => {
     const resp = match[1]; // the captured "whatever"
 
     // send back the matched "whatever" to the chat
-    telegramBot.sendMessage(chatId, resp);
+    bot.sendMessage(chatId, resp);
 });
 
 app.listen(port, () => {
